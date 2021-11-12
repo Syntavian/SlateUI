93657f777dfd54b57b2a9114847cdc89fb71ed8b
Thomas Sloman <syntavian@gmail.com>
2021-11-12 10:52:49 +1000

    Fixed sticky element z index order

---
+++ js/sticky_styles.js
@@ -0,0 +1,39 @@
+import { getElementStyleAsNumber, getElementStylesAsNumberSum } from './utils.js';
+
+let sticky = document.querySelectorAll(".sticky");
+
+function updateBanners() {
+    for (let element of sticky) {
+        let elementStyle = getComputedStyle(element);
+        if (elementStyle.position != "fixed") {
+            if (element.getBoundingClientRect().top < 0) {
+                element.parentElement.style.height = getElementStylesAsNumberSum(elementStyle.height, elementStyle.marginTop, elementStyle.marginBottom, elementStyle.paddingTop, elementStyle.paddingBottom) + "px";
+                element.parentElement.style.width = getElementStylesAsNumberSum(elementStyle.width, elementStyle.marginRight, elementStyle.marginLeft) + "px";
+                element.style.position = "fixed";
+                element.style.zIndex = 99;
+                elementStyle = getComputedStyle(element);
+                element.setAttribute("originalpostion", getElementStyleAsNumber(elementStyle.top));
+                element.style.top = "0px";
+            }
+        } else {
+            element.style.top = "0px";
+            element.parentElement.style.height = getElementStylesAsNumberSum(elementStyle.height, elementStyle.marginTop, elementStyle.marginBottom, elementStyle.paddingTop, elementStyle.paddingBottom) + "px";
+            element.parentElement.style.width = getElementStylesAsNumberSum(elementStyle.width, elementStyle.marginRight, elementStyle.marginLeft) + "px";
+            if (window.scrollY <= Number(element.getAttribute("originalpostion"))) {
+                element.style.top = Number(element.getAttribute("originalpostion")) - window.scrollY + "px";
+            }
+        }
+    }
+}
+
+document.addEventListener("scroll", function (e) {
+    updateBanners();
+}, {
+    passive: false
+});
+
+window.addEventListener("resize", function (e) {
+    updateBanners();
+}, {
+    passive: false
+});
