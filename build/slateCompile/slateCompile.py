bc8d7077e026c1545577d7baf48d0114767b5ebc
Thomas Sloman <syntavian@gmail.com>
2022-01-07 23:35:07 +1000

    Implement custom compiler

--- build/slateCompile/slateCompile.py
+++ build/slateCompile/slateCompile.py
@@ -6,28 +6,60 @@ SLATE_DIR = "./build/css"
 OUTPUT_DIR = "./build/compile"
 
 if __name__ == "__main__":
+
+    # A set of both id and class selectors that must be compiled
+    styleSelectors = set()
+
+    # Analyse HTML files for style ids and classes
     for (dirpath, dirnames, filenames) in os.walk(HTML_DIR):
         sub_dir = ""
 
         if dirpath != HTML_DIR:
             sub_dir = dirpath.replace(HTML_DIR, "")
         for html_file_name in [filename for filename in filenames if os.path.splitext(filename)[1] == ".html"]:
-            html = open(dirpath + "\\" + html_file_name, "r")
-            print(f"\nWorking on {html_file_name}\n")
-            page_file_text = ""
+            html_file = open(dirpath + "\\" + html_file_name, "r")
+            html_file_text = ""
 
-            for page_file_line in html.readlines():
-                page_file_text = page_file_text + page_file_line
-            html.close()
+            for html_file_line in html_file.readlines():
+                html_file_text = html_file_text + html_file_line
+            html_file.close()
 
-            print(page_file_text)
-            
-            styleClassIndices = (styleClassIndex.start() for styleClassIndex in re.finditer("class=", page_file_text))
-            styleClasses = set()
+            styleSelectorIndices = (styleClassIndex.start() for styleClassIndex in re.finditer(r"class=|id=", html_file_text))
 
-            for styleClassIndex in styleClassIndices:
-                for styleClass in page_file_text[styleClassIndex + 7 : page_file_text.find('"', styleClassIndex + 7)].split():
-                    styleClasses.add(styleClass)
+            for styleSelectorIndex in styleSelectorIndices:
+                for styleSelector in html_file_text[styleSelectorIndex + 7 : html_file_text.find('"', styleSelectorIndex + 7)].split():
+                    styleSelectors.add(styleSelector)
 
-            for sc in styleClasses:
-                print(sc)
+    input_css = open(SLATE_DIR + "/style.css", "r")
+    output_css_file_text = ""
+
+    isInRequiredBlock = False
+    blockLevel = 0
+    activeBlockLevel = 0
+
+    for input_css_file_line in input_css.readlines():
+        if blockLevel == activeBlockLevel and (any([(styleSelector in input_css_file_line) for styleSelector in styleSelectors])):
+            isInRequiredBlock = True
+        if '@' in input_css_file_line:
+            isInRequiredBlock = False
+            activeBlockLevel += 1
+            if 'keyframes' in input_css_file_line:
+                isInRequiredBlock = True
+            else:
+                output_css_file_text = output_css_file_text + input_css_file_line
+        if ('{' in input_css_file_line):
+            blockLevel += 1
+        if isInRequiredBlock:
+            output_css_file_text = output_css_file_text + input_css_file_line
+        if ('}' in input_css_file_line):
+            blockLevel -= 1
+            if blockLevel < activeBlockLevel:
+                output_css_file_text = output_css_file_text + input_css_file_line
+            if blockLevel <= activeBlockLevel:
+                activeBlockLevel = blockLevel
+                isInRequiredBlock = False
+
+    input_css.close()
+
+    output_css = open(OUTPUT_DIR + "/style.css", "w")
+    output_css.write(output_css_file_text)
