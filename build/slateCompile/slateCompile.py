ac5b00d051aab662f1fcab5139555ca1e795ca8b
Thomas Sloman <syntavian@gmail.com>
2022-01-11 22:40:16 +1000

    work on compiler

--- build/slateCompile/slateCompile.py
+++ build/slateCompile/slateCompile.py
@@ -4,13 +4,30 @@ import re
 HTML_DIR = "./build"
 SLATE_DIR = "./build/css"
 OUTPUT_DIR = "./build/compile"
-
-#[@\.]([a-zA-Z][\w-]*?)[ ,{]
+JS_REQUIRED_SELECTORS = [
+    "sticky",
+    "div",
+    "sticky-placeholder",
+    "theme-selector",
+    "select",
+    "span",
+    "option",
+    "active",
+    "inactive",
+    "placeholder",
+    "button",
+    "fill",
+    "disabled",
+    "image",
+    "image-carousel",
+    "scroll-bar",
+    "scroll-handle",
+]
 
 if __name__ == "__main__":
 
     # A set of both id and class selectors that must be compiled
-    styleSelectors = set()
+    styleSelectors = set(JS_REQUIRED_SELECTORS)
 
     # Analyse HTML files for style ids and classes
     for (dirpath, dirnames, filenames) in os.walk(HTML_DIR):
@@ -38,33 +55,36 @@ if __name__ == "__main__":
     output_css_file_text = ""
 
     isInRequiredBlock = False
+    isInKeyframesBlock = True
     blockLevel = 0
     activeBlockLevel = 0
 
     for input_css_file_line in input_css.readlines():
         line = input_css_file_line.strip()
         if len(line) > 0 and line[-1] == r'{':
-            matches = set([a.group(1) for a in  re.finditer(r"(?:[\.# ]?([a-zA-Z][\w-]*?)[ ,.#:>])", input_css_file_line)])
+            matches = set([a.group(1) for a in  re.finditer(r"(?:[\.# ]?([a-zA-Z][\w-]*?)[ ,.#:>+])", input_css_file_line)])
             if len(matches) > 0 and any((m in styleSelectors) for m in matches):
                 isInRequiredBlock = True
         if '@' in input_css_file_line:
             isInRequiredBlock = False
             activeBlockLevel += 1
             if 'keyframes' in input_css_file_line:
-                isInRequiredBlock = True
+                isInKeyframesBlock = True
             else:
                 output_css_file_text = output_css_file_text + input_css_file_line
         if ('{' in input_css_file_line):
             blockLevel += 1
-        if isInRequiredBlock:
+        if isInRequiredBlock or isInKeyframesBlock:
             output_css_file_text = output_css_file_text + input_css_file_line
         if ('}' in input_css_file_line):
             blockLevel -= 1
-            if blockLevel < activeBlockLevel:
+            if blockLevel < activeBlockLevel and not isInKeyframesBlock:
                 output_css_file_text = output_css_file_text + input_css_file_line
             if blockLevel <= activeBlockLevel:
                 activeBlockLevel = blockLevel
                 isInRequiredBlock = False
+            if blockLevel == 0: 
+                isInKeyframesBlock = False
 
     input_css.close()
 
