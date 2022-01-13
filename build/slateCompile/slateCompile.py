88ad549c1ceca492c530205e09a66edc7eed933d
Thomas Sloman <syntavian@gmail.com>
2022-01-13 20:08:18 +1000

    fix compiler to achieve parity

--- build/slateCompile/slateCompile.py
+++ build/slateCompile/slateCompile.py
@@ -4,6 +4,9 @@ import re
 HTML_DIR = "./build"
 SLATE_DIR = "./build/css"
 OUTPUT_DIR = "./build/compile"
+CSS_REQUIRED_SELECTORS = [
+    "*",
+]
 JS_REQUIRED_SELECTORS = [
     "sticky",
     "div",
@@ -23,11 +26,14 @@ JS_REQUIRED_SELECTORS = [
     "scroll-bar",
     "scroll-handle",
 ]
+REQUIRED_STYLES = []
+REQUIRED_STYLES.extend(CSS_REQUIRED_SELECTORS)
+REQUIRED_STYLES.extend(JS_REQUIRED_SELECTORS)
 
 if __name__ == "__main__":
 
     # A set of both id and class selectors that must be compiled
-    style_selectors = set(JS_REQUIRED_SELECTORS)
+    style_selectors = set(REQUIRED_STYLES)
 
     # Analyse HTML files for style ids and classes
     for (dir_path, dir_names, file_names) in os.walk(HTML_DIR):
@@ -54,26 +60,34 @@ if __name__ == "__main__":
     input_css = open(SLATE_DIR + "/style.css", "r")
     output_css_file_text = ""
 
+    keyframes_blocks = {}
     media_blocks = {}
 
     is_in_required_block = False
+
     is_in_keyframes_block = False
+    current_keyframes_block = ""
+
     is_in_media_block = False
     current_media_block = ""
+
     block_level = 0
     active_block_level = 0
 
     for input_css_file_line in input_css.readlines():
         line = input_css_file_line.strip()
         if len(line) > 0 and line[-1] == r'{':
-            matches = set([a.group(1) for a in  re.finditer(r"(?:[\.# ]?([a-zA-Z][\w-]*?)[ ,.#:>+])", input_css_file_line)])
-            if len(matches) > 0 and any((m in style_selectors) for m in matches):
+            matches = set([(a.group(1), a.group(2)) for a in  re.finditer(r"(?:[\.# ]?([a-zA-Z][\w-]*?)[ ,.#:>+])|^(\*) ", input_css_file_line)])
+            if len(matches) > 0 and any((m[0] in style_selectors or m[1] in style_selectors) for m in matches):
                 is_in_required_block = True
         if '@' in input_css_file_line:
             is_in_required_block = False
             active_block_level += 1
             if "keyframes" in input_css_file_line:
                 is_in_keyframes_block = True
+                current_keyframes_block = input_css_file_line
+                if current_keyframes_block not in keyframes_blocks.keys():
+                    keyframes_blocks[current_keyframes_block] = ''
             elif "media" in input_css_file_line:
                 is_in_media_block = True
                 current_media_block = input_css_file_line
@@ -85,7 +99,9 @@ if __name__ == "__main__":
             block_level += 1
         if is_in_media_block and is_in_required_block:
             media_blocks[current_media_block] = media_blocks[current_media_block] + input_css_file_line
-        elif is_in_required_block or is_in_keyframes_block:
+        elif is_in_keyframes_block:
+            keyframes_blocks[current_keyframes_block] = keyframes_blocks[current_keyframes_block] + input_css_file_line
+        elif is_in_required_block:
             output_css_file_text = output_css_file_text + input_css_file_line
         if ('}' in input_css_file_line):
             block_level -= 1
@@ -100,6 +116,9 @@ if __name__ == "__main__":
 
     for media_selector, media_block in media_blocks.items():
         output_css_file_text = output_css_file_text + media_selector + media_block + '}\n'
+
+    for keyframes_selector, keyframes_block in keyframes_blocks.items():
+        output_css_file_text = output_css_file_text + keyframes_block
 
     input_css.close()
 
