800d8cfa5e256f5e807c002a56f3898d84e80a6f
Thomas Sloman <syntavian@gmail.com>
2022-02-03 21:55:39 +1000

    completed basic server implementation

--- build/app/lib/app.js
+++ build/app/lib/app.js
@@ -1,9 +1,30 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  fs.createReadStream('public/index.html').pipe(res);
-});
+const publicPath = "./public";
+const extensionContent = {
+    ".html": "text/html", 
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpg",
+    ".wav": "audio/wav",
+};
 
-server.listen(process.env.PORT || 3000);
+http.createServer((request, response) => {
+    const contentPath = (request.url === "/") ? publicPath + "/index.html" : publicPath + request.url;
+    
+    fs.readFile(contentPath, (error, content) => {
+        if (error) {
+            response.writeHead(500);
+            response.end(`Error: ${error}`);
+        } else {
+            response.writeHead(200, { "Content-Type": extensionContent[path.extname(contentPath)] });
+            response.end(content, "utf-8");
+        }
+    });
+}).listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
