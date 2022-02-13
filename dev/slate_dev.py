570b8a5fb83e0ae195daea57d035576106a08f56
Thomas Sloman <syntavian@gmail.com>
2022-02-13 21:15:22 +1000

    move generated project signature to signature_generator file

--- dev/slate_dev.py
+++ dev/slate_dev.py
@@ -7,34 +7,7 @@ from python.string_utils import *
 from python.build import build
 from python.thread_handler import ThreadHandler
 from python.build_event_handler import ModifiedEventBuildEventHandler
-from python.signature_generator import SIGNATURE_IGNORE
-
-# The required Slate project structure to successfully build.
-SIGNATURE: dict[str, list[str]] = {
-    r".": ['build', 'dev'],
-    r".\build": ['app'],
-    r".\build\app": ['lib', 'public', 'slate', 'src'],
-    r".\build\app\lib": [],
-    r".\build\app\public": ['css', 'img', 'js', 'index.html'],
-    r".\build\app\public\css": ['slate.css'],
-    r".\build\app\public\img": [],
-    r".\build\app\public\js": ['slate.js'],
-    r".\build\app\slate": ['slate.css', 'slate.html', 'slate.py'],
-    r".\build\app\src": ['public', 'app.js'],
-    r".\build\app\src\public": ['css', 'html', 'js'],
-    r".\build\app\src\public\css": ['style.css'],
-    r".\build\app\src\public\html": ['components', 'pages'],
-    r".\build\app\src\public\html\pages": ['index.html'],
-    r".\build\app\src\public\js": ['index.js'],
-    r".\dev": ['css', 'js', 'python', 'scss', 'slate_dev.py'],
-    r".\dev\css": ['style.css'],
-    r".\dev\js": ['lib', 'src'],
-    r".\dev\js\lib": [],
-    r".\dev\js\src": ['button_styles.js', 'image_styles.js', 'layout_styles.js', 'scroll_styles.js', 'sticky_styles.js', 'theme_styles.js', 'utils.js'],
-    r".\dev\python": ['build.py', 'build_event_handler.py', 'css_builder.py', 'directories.py', 'find_required_styles.py', 'html_builder.py', 'html_templating.py', 'js_builder.py', 'signature_generator.py', 'string_utils.py', 'thread_handler.py', 'transfer_file.py'],
-    r".\dev\scss": ['elements', 'style.scss', '_borders.scss', '_fixed.scss', '_layout.scss', '_offsets.scss', '_prototypes.scss', '_shadow.scss', '_text.scss', '_variables.scss', '_z-index.scss'],
-    r".\dev\scss\elements": ['_backgrounds.scss', '_box.scss', '_buttons.scss', '_canvas.scss', '_fade.scss', '_floating.scss', '_forms.scss', '_images.scss', '_links.scss', '_popup.scss'],
-}
+from python.signature_generator import SIGNATURE_IGNORE, SIGNATURE
 
 if __name__ == "__main__":
     # Verify the project structure.
