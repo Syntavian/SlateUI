2ec7717009db6d427c7e91c12af17981d3b34fab
Thomas Sloman <syntavian@gmail.com>
2022-03-08 21:27:23 +1000

    test repeat function

--- dev/python/utils/test_string_utils.py
+++ dev/python/utils/test_string_utils.py
@@ -13,3 +13,13 @@ class TestRedactOverflow:
         assert redact_overflow("abcdefghijklmnopqrstuvwxyz", 5) == "abcde..."
     def test_redact_overflow_same(self):
         assert redact_overflow("abcdefghijklmnopqrstuvwxyz", 100) == "abcdefghijklmnopqrstuvwxyz"
+
+class TestRepeat:
+    def test_repeat(self):
+        assert repeat("abc", 3) == "abc\nabc\nabc"
+    def test_repeat_no_separator(self):
+        assert repeat("abc", 3, '') == "abcabcabc"
+    def test_repeat_end(self):
+        assert repeat("abc", 3, '', 'end') == "abcabcabcend"
+    def test_repeat_end_newline(self):
+        assert repeat("abc", 3, _end='end') == "abc\nabc\nabcend"
