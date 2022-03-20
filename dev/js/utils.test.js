988b0a64d55fa7dd3b9c26c3bdd0d29456b1d4cb
Thomas Sloman <syntavian@gmail.com>
2022-03-20 21:54:23 +1000

    Add more tests for js utils

--- dev/js/utils.test.js
+++ dev/js/utils.test.js
@@ -1,13 +1,25 @@
 import { getElementStyleAsNumber, getElementStylesAsNumberSum } from "./utils";
 
-describe( "getElementStyleAsNumber()", () => {
+describe("getElementStyleAsNumber()", () => {
     test("should remove 'px' and return the number", () => {
         expect(getElementStyleAsNumber("0px")).toBe(0);
+    });
+    test("should remove 'px' and return the number", () => {
+        expect(getElementStyleAsNumber("20px")).toBe(20);
+    });
+    test("should remove 'px' and return the number", () => {
+        expect(getElementStyleAsNumber("03px")).toBe(3);
+    });
+    test("should remove 'px' and return the number", () => {
+        expect(getElementStyleAsNumber("4")).toBe(4);
     });
 });
 
 describe("getElementStylesAsNumberSum()", () => {
     test("should remove all 'px' and return the sum of all numbers", () => {
         expect(getElementStylesAsNumberSum("0px", "1px", "2px")).toBe(3);
+    });
+    test("should remove all 'px' and return the sum of all numbers", () => {
+        expect(getElementStylesAsNumberSum("", "3", "4px")).toBe(7);
     });
 });
