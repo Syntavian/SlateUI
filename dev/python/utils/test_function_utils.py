332bf950b909cae6221ef551ff9e5fd28f63b86c
Thomas Sloman <syntavian@gmail.com>
2022-03-22 20:16:08 +1000

    add python test for repeat function

---
+++ dev/python/utils/test_function_utils.py
@@ -0,0 +1,10 @@
+from python.utils.function_utils import *
+
+
+class TestRepeatFunction:
+    def test_repeat_function(self):
+        assert repeat_function(
+            lambda a, b: a * b,
+            2,
+            [(1, 2), (3, 4)]
+        ) == [2, 12]
